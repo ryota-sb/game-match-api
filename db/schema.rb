5cb807454c25633aca5555ed3bdfdec9e70e8db4
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-14 08:37:01 +0900

    profile model 作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_02_071309) do
+ActiveRecord::Schema.define(version: 2021_08_13_233456) do
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "discord_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "sub"
