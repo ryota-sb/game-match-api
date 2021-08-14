0b27f0d2d411942a08754b9e48beace1f7bb3d27
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-14 16:05:36 +0900

    profile route 追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :secured, only: [:index]
       resources :test, only: [:index]
+      resources :profiles, only: [:create, :update]
     end
   end
 end
