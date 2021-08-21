11dd9fa3364653917b154de627e2410c3ed84601
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-21 20:06:32 +0900

    swipes controller 追加

--- app/controllers/Api/V1/swipes_controller.rb
+++ app/controllers/Api/V1/swipes_controller.rb
@@ -1,6 +1,32 @@
 module Api
   module V1
     class SwipesController < ApplicationController
+      
+      def index
+        # すでにswipeしたuserを省いて、swipeしていないuserを１つ取得
+        
+        # swipeしたuser取得して配列に入れる
+        # swiped_user_id = Swipe.where(from_user_id: current_user.id).pluck(:to_user_id)
+        # render json: { data: swiped_user_id }
+
+        data = current_user.reverse_of_swipes
+        render json: { data: data }
+      end
+
+      def create
+        
+      end
+
+      def destroy
+      
+      end
+
+      private
+
+      def swipe_params
+        params.require(:swipe).permit(:to_user_id, :from_user_id, :is_like)
+      end
+      
     end
   end
 end
