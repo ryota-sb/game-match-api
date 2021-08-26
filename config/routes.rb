0669ae3d36f7346a49c256f522177e65a9177d72
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-26 20:06:13 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'login', to: 'secured#login'
       get 'logout', to: 'secured#logout'
-      resources :users, only: [:index]
+      resources :users, only: [:index, :show]
       resources :profiles, only: [:create, :update]
+      resources :swipes, only: [:index]
     end
   end
 end
