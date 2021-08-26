3b23af298c0048a9e464d3b3ab97e6e32504d16e
ryota-sb <ryota.sb.1206@gmail.com>
2021-08-26 19:57:42 +0900

    profilesテーブルにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_082025) do
+ActiveRecord::Schema.define(version: 2021_08_26_105429) do
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "discord_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_082025) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
